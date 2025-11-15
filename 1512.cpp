fbaae35ccb56eb7fb7bdcac3c5cf8e891ba783f7
cayoo <krzysztof.turek007@gmail.com>
2025-11-15 18:41:03 +0100

    new solutions

---
+++ 1512.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int numIdenticalPairs(vector<int>& nums) {
+		int ans = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			for (int j = i + 1; j < nums.size(); j++) {
+				if (nums[i] == nums[j]) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+};
+
+
+int main() {
+	Solution solution;
+
+	vector<int> nums = {1, 2, 3, 1, 1, 3};
+
+	int ans = solution.numIdenticalPairs(nums);
+
+	cout << ans << endl;
+
+	return 0;
+}
