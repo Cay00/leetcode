a0603342f7c1416230282744ee9f4da9a44335f0
cayoo <krzysztof.turek007@gmail.com>
2025-11-16 00:11:34 +0100

    solutions

---
+++ 3005.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxFrequencyElements(vector<int>& nums) {
+		int res = 0;
+
+		vector<int> freq(101, 0);
+
+		for (int num : nums) {
+			freq[num]++;
+		}
+		
+		int maxR = 0;
+		for (int f : freq) {
+			maxR = max(maxR, f);
+		}
+
+		for (int f : freq) {
+			if (f == maxR) {
+				res += maxR;
+			}
+		}
+	
+		return res;
+	}
+};
+
+
+int main() {
+	Solution solution;
+
+	vector<int> nums = { 1,2,2,3,1,4 };
+	int ans = solution.maxFrequencyElements(nums);
+	cout << ans << endl;
+
+	return 0;
+}
---
+++ 3234.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// time esceeded 
+
+class Solution {
+public:
+	int numberOfSubstrings(string s) {
+		int n = s.size();
+		int res = 0;
+
+		for (int i = 0; i < n; i++) {
+			int zero = 0;
+			int one = 0;
+			for (int j = i; j < n; j++) {
+				if (s[j] == '0') {
+					zero++;
+					if (zero >= 200) break;
+				}
+				else {
+					one++;
+					if (one >= 39801) {
+						res++;
+						break;
+					}
+				}
+
+				if (one >= zero * zero) {
+					res++;
+				}
+			}
+			
+		}
+		return res;
+	}
+};
+
+
+int main() {
+	Solution solution;
+
+	string s = "00011";
+	int ans = solution.numberOfSubstrings(s);
+	cout << ans << endl;
+
+	return 0;
+}
