4440164ef0eb3bba2d4470f2bd121a80712f22c3
cayoo <krzysztof.turek007@gmail.com>
2025-11-19 00:09:35 +0100

    solutions

---
+++ 14.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = strs[0];
+
+        for (string str : strs) {
+            while (str.find(res) != 0) {
+                res = res.substr(0, res.length() - 1);
+			}
+        }
+		
+		return res;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<string> strs = { "flower","flow","flight" };
+    string ans = solution.longestCommonPrefix(strs);
+    
+    cout << ans << endl;
+
+    return 0;
+}
