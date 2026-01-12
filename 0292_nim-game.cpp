1bfc932bb88c6048737ab8e5fee22735ca376955
cayoo <krzysztof.turek007@gmail.com>
2026-01-12 23:39:42 +0100

    solutions

---
+++ 0292_nim-game.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }
+        else
+            return true;
+    }
+};
+
+int main() {
+	Solution solution;
+
+	return 0;
+}
---
+++ 1266_minimum-time-visiting-all-points.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+
+        for (int i = 0; i < points.size() - 1; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+
+            int x_target = points[i + 1][0];
+            int y_target = points[i + 1][1];
+
+            ans += max(abs(x_target - x), abs(y_target - y));
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+	Solution solution;
+
+	return 0;
+}
