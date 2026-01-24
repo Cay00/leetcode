134df0840177dca02afd4e13c05e7eefd0d11440
cayoo <krzysztof.turek007@gmail.com>
2026-01-25 00:33:47 +0100

    solutions

---
+++ 1877_minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        int ans = 0;
+
+        sort(nums.begin(), nums.end());
+
+        int x = 1;
+        for (int i = 0; i < nums.size() / 2; i++) {
+            int last = nums.size() - x;
+            x++;
+
+            if (nums[i] + nums[last] > ans)
+                ans = nums[i] + nums[last];
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+	//Solution solution;
+
+	return 0;
+}
